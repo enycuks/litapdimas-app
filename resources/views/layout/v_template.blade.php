65f776562cfbf566e780022b7deff690aea6cf1f
enycuks <enycuks@gmail.com>
2021-12-21 19:11:24 +0700

    template fix

--- resources/views/layout/v_template.blade.php
+++ resources/views/layout/v_template.blade.php
@@ -505,9 +505,7 @@
                 <div class="container-fluid">
                     <div class="row">
                         <div class="col-sm-6">
-                            <script>
-                                document.write(new Date().getFullYear())
-                            </script> © Minible.
+                           
                         </div>
                         <div class="col-sm-6">
                             <div class="text-sm-end d-none d-sm-block">
@@ -555,7 +553,7 @@
     
 
     <!-- App js -->
-    <script src={{ asset('template') }}/"assets/js/app.js"></script>
+    <script src="{{ asset('template') }}/assets/js/app.js"></script>
 
 </body>
 
