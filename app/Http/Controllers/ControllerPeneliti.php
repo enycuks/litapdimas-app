b6fe1d747ef05e8dff50f42ef740429736ed9cf9
enycuks <enycuks@gmail.com>
2022-03-11 19:37:10 +0700

    test

--- app/Http/Controllers/ControllerPeneliti.php
+++ app/Http/Controllers/ControllerPeneliti.php
@@ -19,21 +19,11 @@ class ControllerPeneliti extends Controller
 
     public function proposal2()
     {
-        $data['klas'] = Http::get('https://litapdimas.kemenag.go.id/index.php/api/kategori/2022/')->json();
-        $data['bidang'] = Http::get('https://litapdimas.kemenag.go.id/index.php/api/bidangilmu/')->json();
-        $data['tema'] = Http::get('https://litapdimas.kemenag.go.id/index.php/api/ref_subarkan/')->json();
-        $data['luaran'] = Http::get('https://litapdimas.kemenag.go.id/index.php/api/luaran/')->json();
-        // dd($data);
         return view('/peneliti/proposal-2', $data);
     }
 
     public function proposal3()
     {
-        $data['klas'] = Http::get('https://litapdimas.kemenag.go.id/index.php/api/kategori/2022/')->json();
-        $data['bidang'] = Http::get('https://litapdimas.kemenag.go.id/index.php/api/bidangilmu/')->json();
-        $data['tema'] = Http::get('https://litapdimas.kemenag.go.id/index.php/api/ref_subarkan/')->json();
-        $data['luaran'] = Http::get('https://litapdimas.kemenag.go.id/index.php/api/luaran/')->json();
-        // dd($data);
         return view('/peneliti/proposal-3', $data);
     }
 }
